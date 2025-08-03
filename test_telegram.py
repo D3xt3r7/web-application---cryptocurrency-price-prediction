94e0913e5504fb116afd942524e867ffccbfe2cc
Piotr <piotr.kostrzewa@icloud.com>
2025-08-03 20:10:05 +0200

    test_telegram.py

--- test_telegram.py
+++ test_telegram.py
@@ -2,8 +2,8 @@ from telegram import Bot
 import asyncio
 
 async def test_telegram():
-    bot = Bot(token='8222019926:AAFRMAPFFADKZhiQUD6751t-Cg_IWmleG7g')
-    await bot.send_message(chat_id='8247636582', text='Testowa wiadomość')
+    bot = Bot(token='xxxx')
+    await bot.send_message(chat_id='xxx', text='Testowa wiadomość')
     print("Wiadomość wysłana!")
 
 asyncio.run(test_telegram())
